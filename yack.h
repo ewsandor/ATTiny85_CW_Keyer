1bcf0d69b490fae0aa0e85113779e2aac1a51364
Edward Sandor <public+git@sandorlaboratories.com>
2024-08-17 17:36:25 -0500

    Reduce sleep delay

--- yack.h
+++ yack.h
@@ -86,7 +86,7 @@
 
 // Power save mode
 #define     POWERSAVE       // Comment this line if no power save mode required
-#define     PSTIME          30 // 30 seconds until automatic powerdown
+#define     PSTIME          5 // 5 seconds until automatic powerdown
 #define     PWRWAKE         ((1<<PCINT3) | (1<<PCINT4) | (1<<PCINT2)) // Dit, Dah or Command wakes us up..
 
 // These values limit the speed that the keyer can be set to
